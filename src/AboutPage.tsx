4d2caaeb1ddce4e8ba32ee7bae36cdc8eaffb042
Finisus <finisvisus@gmail.com>
2024-09-02 22:36:55 +0530

    add grid dot bg, add grid pattern bg

--- src/AboutPage.tsx
+++ src/AboutPage.tsx
@@ -1,9 +1,33 @@
+import gridDotsSVG from './assets/grid[circular-dots][18x18].svg';
 
 function AboutPage(): JSX.Element {
-
   return(
     <div className="ap">
+      <div className="gridBackground">
+        <div className='columns'>
+          <div className='rows'>
+            <img src={gridDotsSVG} />
+            <img src={gridDotsSVG} />
+            <img src={gridDotsSVG} />
+            <img src={gridDotsSVG} />
+          </div>
+          <div className='rows'>
+            <img src={gridDotsSVG} />
+            <img src={gridDotsSVG} />
+            <img src={gridDotsSVG} />
+            <img src={gridDotsSVG} />
+          </div>
+          <div className='rows'>
+            <img src={gridDotsSVG} />
+            <img src={gridDotsSVG} />
+            <img src={gridDotsSVG} />
+            <img src={gridDotsSVG} />
+          </div>
+        </div>
+      </div>
 
+      <span>:: About ::</span>
+      <h1>I design</h1>
     </div>
   );
 }
--- src/WorksPage.tsx
+++ src/WorksPage.tsx
@@ -1,9 +1,32 @@
+import gridSmallSVG from './assets/grid[pattern-plus][18x18].svg';
 
 function WorksPage(): JSX.Element {
-
   return(
     <div className="wp">
+      <div className="gridBackground">
+        <div className='columns'>
+          <div className='rows'>
+            <img src={gridSmallSVG} />
+            <img src={gridSmallSVG} />
+            <img src={gridSmallSVG} />
+            <img src={gridSmallSVG} />
+          </div>
+          <div className='rows'>
+            <img src={gridSmallSVG} />
+            <img src={gridSmallSVG} />
+            <img src={gridSmallSVG} />
+            <img src={gridSmallSVG} />
+          </div>
+          <div className='rows'>
+            <img src={gridSmallSVG} />
+            <img src={gridSmallSVG} />
+            <img src={gridSmallSVG} />
+            <img src={gridSmallSVG} />
+          </div>
+        </div>
+      </div>
 
+      <span>:: Works ::</span>
     </div>
   );
 }
