e6f09f12c6b158f81f33bbcbe5efe26e58f78dcb
Finisus <finisvisus@gmail.com>
2024-09-05 21:58:44 +0530

    add rest of the showcase web links, add mobile support to Showcase

--- src/ShowcaseWeb.tsx
+++ src/ShowcaseWeb.tsx
@@ -1,15 +1,39 @@
 import { useState } from 'react';
 import './ShowcaseWeb.css';
+import loadingBarBgBorder from './assets/customLoadingBar/container-bg-border.png';
+import loadingBar1 from './assets/customLoadingBar/pixel-loading-1.svg';
+import loadingBar2 from './assets/customLoadingBar/pixel-loading-2.svg';
+import loadingBar3 from './assets/customLoadingBar/pixel-loading-3.svg';
+import loadingBar4 from './assets/customLoadingBar/pixel-loading-4.svg';
+import loadingBar5 from './assets/customLoadingBar/pixel-loading-5.svg';
+import loadingBar6 from './assets/customLoadingBar/pixel-loading-6.svg';
+import loadingBar7 from './assets/customLoadingBar/pixel-loading-7.svg';
+import loadingBar8 from './assets/customLoadingBar/pixel-loading-8.svg';
+import loadingBar9 from './assets/customLoadingBar/pixel-loading-9.svg';
+import loadingBar10 from './assets/customLoadingBar/pixel-loading-10.svg';
 
 type ShowcaseType = {
-  meter: number,
+  meter: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10,
   name: string,
   preview: JSX.Element,
   link: string
 }
 
-function ShowcaseWeb({ name, preview, link }: ShowcaseType): JSX.Element {
+function ShowcaseWeb({ meter, name, preview, link }: ShowcaseType): JSX.Element {
   const [linkPopup, setLinkPopup] = useState<string | null>(null);
+
+  const loadingBarImg: { [key: number]: string } = {
+    1: loadingBar1,
+    2: loadingBar2,
+    3: loadingBar3,
+    4: loadingBar4,
+    5: loadingBar5,
+    6: loadingBar6,
+    7: loadingBar7,
+    8: loadingBar8,
+    9: loadingBar9,
+    10: loadingBar10
+  };
 
   return (
     <div 
@@ -17,6 +41,13 @@ function ShowcaseWeb({ name, preview, link }: ShowcaseType): JSX.Element {
       onMouseEnter={() => setLinkPopup(link)} 
       onMouseLeave={() => setLinkPopup(null)} 
     >
+      <div className='loadingBar'>
+        <img className='bg-curved-border' src={loadingBarBgBorder} />
+        <span className='quirkyText'>Quirky</span>
+        <img src={loadingBarImg[meter]} /> 
+        <span className='normalText'>Exquisite</span>
+      </div>
+      
       <div 
         className="preview"
         onClick={() => { window.open(`${link}`, "_blank") }}  
--- src/WorksPage.tsx
+++ src/WorksPage.tsx
@@ -18,8 +18,26 @@ import billyPreview from './assets/workPreviews/billy-preview[640x359].png';
 import y2kPreview from './assets/workPreviews/y2k-preview[640x359].png';
 import chomikPreview from './assets/workPreviews/chomik-preview[640x359].png';
 import wechoosewealthPreview from './assets/workPreviews/we-choose-wealth-preview[640x359].png';
+import pizzagatePreview from './assets/workPreviews/pizzagate-preview[640x359].png';
+import solanacartelPreview from './assets/workPreviews/solana-cartel-preview[640x359].png';
+import failanaPreview from './assets/workPreviews/failana-preview[640x359].png';
+import lethimcookPreview from './assets/workPreviews/lethimcook-preview[640x359].png';
+import skbdiPreview from './assets/workPreviews/skbdi-preview[640x359].png';
+import catwifgoldPreview from './assets/workPreviews/catwifgold-preview[640x359].png';
+import torocatPreview from './assets/workPreviews/torocat-preview[640x359].png';
+import lickcatPreview from './assets/workPreviews/lickcat-preview[640x359].png';
+import roaringpixelkittyPreview from './assets/workPreviews/roaring-pixel-kitty-preview[640x359].png';
+import lvdogPreview from './assets/workPreviews/lvdog-preview[640x359].png';
+import mochadogPreview from './assets/workPreviews/mocha-dog-preview[640x359].png';
+import reetardsPreview from './assets/workPreviews/reetards-preview[640x359].png';
+import ricardomemePreview from './assets/workPreviews/ricardo-meme-preview[640x359].png';
+import nyanpopPreview from './assets/workPreviews/nyanpop-preview[640x359].png';
+import shirocatPreview from './assets/workPreviews/shiro-cat-preview[640x359].png';
 
-const preloadSrcList: string[] = [gridSmallSVG, htmlIconSVG, cssIconSVG, jsIconSVG, reactIconSVG, tsIconSVG, ethIconSVG, ceicatPreview, ceicatPfpGenPreview, billyPreview, y2kPreview];
+const preloadSrcList: string[] = [gridSmallSVG, htmlIconSVG, cssIconSVG, jsIconSVG, reactIconSVG, tsIconSVG, ethIconSVG, 
+  ceicatPreview, ceicatPfpGenPreview, billyPreview, y2kPreview, pizzagatePreview, solanacartelPreview, failanaPreview, 
+  lethimcookPreview, skbdiPreview, catwifgoldPreview, torocatPreview, lickcatPreview, roaringpixelkittyPreview, lvdogPreview,
+  mochadogPreview, reetardsPreview, ricardomemePreview, nyanpopPreview, shirocatPreview ];
 
 function WorksPage(): JSX.Element {
   const { imagesPreloaded } = useImagePreloader(preloadSrcList);
@@ -119,47 +137,151 @@ function WorksPage(): JSX.Element {
         <div className='showcase-container'>
 
           <ShowcaseWeb 
-            meter={90}
+            meter={10}
             preview={<img src={ceicatPreview} />} 
             name={"$CEICAT - Ceiling Cat"}
             link={"https://finisus-ceicat.vercel.app/"}
           />
 
           <ShowcaseWeb 
-            meter={50} 
+            meter={10} 
             preview={<img src={ceicatPfpGenPreview} />}
             name={"Ceiling Cat's PFP Gen"}
             link={"https://finisus-ceicat-pfpgen.vercel.app/"}
           />
 
           <ShowcaseWeb
-            meter={50}
+            meter={4}
             preview={<img src={billyPreview} />}
             name={"$BILLY - My rendition"}
             link={"https://finisus-billy.vercel.app/"}
           />
 
           <ShowcaseWeb
-            meter={50}
+            meter={8}
             preview={<img src={y2kPreview} />}
             name={"Y2K"}
             link={"https://finisus-y2k.vercel.app/"}
           />
 
           <ShowcaseWeb
-            meter={50}
+            meter={1}
             preview={<img src={chomikPreview} />}
             name={"Chomik"}
             link={"https://finisus-chomik.vercel.app/"}
           /> 
 
           <ShowcaseWeb
-            meter={50}
+            meter={7}
             preview={<img src={wechoosewealthPreview} />}
-            name={"Chomik"}
+            name={"$WEALTH - We Choose Wealth"}
             link={"https://finisus-wechosewealth.vercel.app/"}
           /> 
 
+          <ShowcaseWeb
+            meter={4}
+            preview={<img src={pizzagatePreview} />}
+            name={"$PIZZAGATE"}
+            link={"https://finisus-pizzagate.vercel.app/"}
+          /> 
+
+          <ShowcaseWeb
+            meter={5}
+            preview={<img src={solanacartelPreview} />}
+            name={"$CARTEL - Solana Cartel"}
+            link={"https://finisus-solanacartel.vercel.app/"}
+          /> 
+
+          <ShowcaseWeb
+            meter={8}
+            preview={<img src={failanaPreview} />}
+            name={"$FAILANA"}
+            link={"https://finisus-failana.vercel.app/"}
+          /> 
+
+          <ShowcaseWeb
+            meter={3}
+            preview={<img src={lethimcookPreview} />}
+            name={"$COOK - Let him cook"}
+            link={"https://finisus-lethimcook.vercel.app/"}
+          /> 
+
+          <ShowcaseWeb
+            meter={1}
+            preview={<img src={skbdiPreview} />}
+            name={"$SKBDI"}
+            link={"https://finisus-skbdi.vercel.app/"}
+          /> 
+
+          <ShowcaseWeb
+            meter={10}
+            preview={<img src={catwifgoldPreview} />}
+            name={"Cat wif gold"}
+            link={"https://finisus-catwifgold.vercel.app/"}
+          /> 
+
+          <ShowcaseWeb
+            meter={8}
+            preview={<img src={torocatPreview} />}
+            name={"$TORO - Toro Inoue"}
+            link={"https://finisus-toroinoue.vercel.app/"}
+          /> 
+
+          <ShowcaseWeb
+            meter={6}
+            preview={<img src={lickcatPreview} />}
+            name={"$LICK - Lick Cat"}
+            link={"https://finisus-lickcat.vercel.app/"}
+          /> 
+
+          <ShowcaseWeb
+            meter={7}
+            preview={<img src={roaringpixelkittyPreview} />}
+            name={"$RPK - Roaring Pixel Kitty"}
+            link={"https://finisus-roaringpixelkitty.vercel.app/"}
+          /> 
+
+          <ShowcaseWeb
+            meter={6}
+            preview={<img src={lvdogPreview} />}
+            name={"louis Vuitton Dog"}
+            link={"https://finisus-lvdog.vercel.app/"}
+          /> 
+
+          <ShowcaseWeb
+            meter={5}
+            preview={<img src={mochadogPreview} />}
+            name={"Mocha Dog"}
+            link={"https://finisus-mochadog.vercel.app/"}
+          /> 
+
+          <ShowcaseWeb
+            meter={1}
+            preview={<img src={reetardsPreview} />}
+            name={"Reetards"}
+            link={"https://finisus-reetards.vercel.app/"}
+          /> 
+
+          <ShowcaseWeb
+            meter={1}
+            preview={<img src={ricardomemePreview} />}
+            name={"$RICARDO"}
+            link={"https://finisus-ricardo.vercel.app/"}
+          /> 
+
+          <ShowcaseWeb
+            meter={7}
+            preview={<img src={nyanpopPreview} />}
+            name={"Nyan Pop Cat"}
+            link={"https://finisus-nyanpop.vercel.app/"}
+          /> 
+
+          <ShowcaseWeb
+            meter={7}
+            preview={<img src={shirocatPreview} />}
+            name={"Shiro Cat"}
+            link={"https://finisus-shirocat.vercel.app/"}
+          /> 
         </div>
       </div>
       <Info />
